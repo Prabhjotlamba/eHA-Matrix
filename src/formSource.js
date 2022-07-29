48e8b272ea8e067d37d3af5ed0c4871302030b45
Prabhjyot Kaur <prabhjyot@techverse.world>
2022-07-29 13:14:43 +0530

    Dashboard first view

---
+++ src/formSource.js
@@ -0,0 +1,77 @@
+export const userInputs = [
+    {
+      id: 1,
+      label: "Username",
+      type: "text",
+      placeholder: "Aman",
+    },
+    {
+      id: 2,
+      label: "Name and surname",
+      type: "text",
+      placeholder: "Aman",
+    },
+    {
+      id: 3,
+      label: "Email",
+      type: "mail",
+      placeholder: "Aman@gmail.com",
+    },
+    {
+      id: 4,
+      label: "Phone",
+      type: "text",
+      placeholder: "+91 234 567 1891",
+    },
+    {
+      id: 5,
+      label: "Password",
+      type: "password",
+    },
+    {
+      id: 6,
+      label: "Address",
+      type: "text",
+      placeholder: "Raipur",
+    },
+    {
+      id: 7,
+      label: "Country",
+      type: "text",
+      placeholder: "India",
+    },
+  ];
+  
+  export const productInputs = [
+    {
+      id: 1,
+      label: "Title",
+      type: "text",
+      placeholder: "Apple Macbook Pro",
+    },
+    {
+      id: 2,
+      label: "Description",
+      type: "text",
+      placeholder: "Description",
+    },
+    {
+      id: 3,
+      label: "Category",
+      type: "text",
+      placeholder: "Computers",
+    },
+    {
+      id: 4,
+      label: "Price",
+      type: "text",
+      placeholder: "100",
+    },
+    {
+      id: 5,
+      label: "Stock",
+      type: "text",
+      placeholder: "in stock",
+    },
+  ];
+  
